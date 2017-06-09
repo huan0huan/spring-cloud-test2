98ef37b7a2616c7b5f439ec83ef048efba2216f5
huan0huan <717404669@qq.com>
2017-06-09 10:38:05 +0800

    添加eurekaServer项目

---
+++ eurekaServer/src/main/java/com/my/EurekaServerApplication.java
@@ -0,0 +1,16 @@
+package com.my;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * Created by admin on 2017/6/9.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class, args);
+    }
+}
